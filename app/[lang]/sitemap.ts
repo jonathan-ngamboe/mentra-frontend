1f23f0e3a91e65d5279eec11399a0f42ccb0b934
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2025-02-27 13:53:23 +0200

    fix: update sitemap generator to handle dynamic language routes

--- app/[lang]/sitemap.ts
+++ app/[lang]/sitemap.ts
@@ -1,12 +1,19 @@
 import { baseURL, routes as routesConfig } from "@/resources/config";
+import { SupportedLocale } from "./dictionaries";
 
-export default async function sitemap() {
+interface SitemapParams {
+  params: { lang: SupportedLocale };
+}
+
+export default async function sitemap({ params }: SitemapParams) {
+  const lang = params.lang;
+  
   const activeRoutes = Object.entries(routesConfig)
     .filter(([_, isActive]) => isActive)
     .map(([route]) => route);
 
   const routes = activeRoutes.map((route) => ({
-    url: `${baseURL}${route !== "/" ? route : ""}`,
+    url: `${baseURL}/${lang}${route !== "/" ? route : ""}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
