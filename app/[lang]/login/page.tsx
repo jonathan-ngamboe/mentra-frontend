9670287d87eb85fc2020d0d9881a8b1e255fe412
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2025-02-27 12:13:53 +0200

    Add localized login page with OTP login component

---
+++ app/[lang]/login/page.tsx
@@ -0,0 +1,29 @@
+import { getDictionary, SupportedLocale } from "../dictionaries";
+import { OtpLogin } from "@/components/OtpLogin";
+import { Logo } from "@/components/Logo"; 
+
+export default async function LoginPage({
+  params,
+}: {
+  params: Promise<{ lang: SupportedLocale }>;
+}) {
+  const lang = (await params).lang;
+  const dictionary = await getDictionary(lang);
+
+  return (
+    <div className="flex justify-center px-12 transform translate-y-1 transition-all w-full">
+      <div className="max-w-sm w-full flex flex-col items-center justify-center gap-8 login-form-wrapper">
+        <div className="flex flex-col items-center text-center gap-6">
+          <Logo icon size="m" />
+          <h1 className="text-2xl font-bold text-center text-balance">
+            {dictionary.login.welcome}
+          </h1>
+          <p className="text-sm text-center text-gray-600">
+            {dictionary.login.otp.description}
+          </p>
+        </div>
+        <OtpLogin dictionary={dictionary} />
+      </div>
+    </div>
+  );
+}
