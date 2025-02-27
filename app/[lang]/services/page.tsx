1614c9ca66edef5a689e9579d31b503e571b546a
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2025-02-27 12:14:22 +0200

    Add Services component with localized service items and FlowingMenu

---
+++ app/[lang]/services/page.tsx
@@ -0,0 +1,27 @@
+import { getDictionary, SupportedLocale } from "../dictionaries";
+import { services } from "@/resources/config";
+import FlowingMenu from "@/components/reactbits/Components/FlowingMenu/FlowingMenu";
+import { getNestedValue } from "@/lib/utils";
+
+export default async function Services({
+  params,
+}: {
+  params: Promise<{ lang: SupportedLocale }>;
+}) {
+  const lang = (await params).lang;
+  const dict = await getDictionary(lang);
+
+  function getServiceItems() {
+    return services.map((service) => ({
+      text: getNestedValue(dict, service.labelKey) || service.labelKey,
+      image: service.image,
+      link: service.link,
+    }));
+  }
+
+  return (
+    <div className="relative w-full self-center">
+      <FlowingMenu items={getServiceItems()} className="text-foreground" />
+    </div>
+  );
+}
