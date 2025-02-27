4da0a67a70c26d73cdc20d6b141ada71e30ba0a0
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2025-02-27 11:59:21 +0200

    Add utility function to retrieve nested object values safely

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getNestedValue(obj: any, path: string): string {
+  return path.split(".").reduce((prev, curr) => {
+    return prev && prev[curr] ? prev[curr] : path;
+  }, obj);
 }
