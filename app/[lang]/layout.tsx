86c1760cf66afbc8f59361aafea700cbd4b33e57
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2025-02-27 20:12:26 +0200

    feat: integrate ViewTransitions and ReactLenis for improved layout transitions

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,29 +1,31 @@
-import { headers } from "next/headers";
-import { Metadata } from "next";
+import { headers } from 'next/headers';
+import { Metadata } from 'next';
 
-import { baseURL, metaByLang } from "@/resources/config";
-import { Inter, Roboto_Mono } from "next/font/google";
-import { Toaster } from "@/components/ui/sonner";
-import { getDictionary, SupportedLocale } from "./dictionaries";
-import { Header } from "@/components/Header";
-import { Footer } from "@/components/Footer";
-import { cn } from "@/lib/utils";
-import { Background } from "@/components/Background";
-import { ThemeProvider } from "@/components/theme-provider";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { baseURL, metaByLang } from '@/resources/config';
+import { Inter, Roboto_Mono } from 'next/font/google';
+import { Toaster } from '@/components/ui/sonner';
+import { getDictionary, SupportedLocale } from './dictionaries';
+import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
+import { cn } from '@/lib/utils';
+import { Background } from '@/components/Background';
+import { ThemeProvider } from '@/components/theme-provider';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { ViewTransitions } from 'next-view-transitions';
+import { ReactLenis } from 'lenis/react'
 
-import "../globals.css";
+import '../globals.css';
 
 const primary = Inter({
-  subsets: ["latin"],
-  variable: "--font-primary",
-  display: "swap",
+  subsets: ['latin'],
+  variable: '--font-primary',
+  display: 'swap',
 });
 
 const code = Roboto_Mono({
-  subsets: ["latin"],
-  variable: "--font-code",
-  display: "swap",
+  subsets: ['latin'],
+  variable: '--font-code',
+  display: 'swap',
 });
 
 export async function generateMetadata({
@@ -31,9 +33,9 @@ export async function generateMetadata({
 }: {
   params: Promise<{ lang: SupportedLocale }>;
 }): Promise<Metadata> {
-  const host = (await headers()).get("host");
+  const host = (await headers()).get('host');
   const metadataBase = host ? new URL(`https://${host}`) : undefined;
-  const lang = (await params).lang || "en";
+  const lang = (await params).lang || 'en';
   const localizedMeta = metaByLang[lang];
 
   return {
@@ -42,7 +44,7 @@ export async function generateMetadata({
     openGraph: {
       title: localizedMeta.og.title,
       description: localizedMeta.og.description,
-      url: "https://" + baseURL,
+      url: 'https://' + baseURL,
       images: [
         {
           url: localizedMeta.og.image,
@@ -50,21 +52,21 @@ export async function generateMetadata({
         },
       ],
       type: localizedMeta.og.type as
-        | "website"
-        | "article"
-        | "book"
-        | "profile"
-        | "music.song"
-        | "music.album"
-        | "music.playlist"
-        | "music.radio_station"
-        | "video.movie"
-        | "video.episode"
-        | "video.tv_show"
-        | "video.other",
+        | 'website'
+        | 'article'
+        | 'book'
+        | 'profile'
+        | 'music.song'
+        | 'music.album'
+        | 'music.playlist'
+        | 'music.radio_station'
+        | 'video.movie'
+        | 'video.episode'
+        | 'video.tv_show'
+        | 'video.other',
     },
     twitter: {
-      card: "summary_large_image",
+      card: 'summary_large_image',
       title: localizedMeta.og.title,
       description: localizedMeta.og.description,
       images: [localizedMeta.og.image],
@@ -84,32 +86,31 @@ export default async function RootLayout({
   const dict = await getDictionary(lang);
 
   return (
-    <html
-      lang={lang}
-      className={cn(
-        primary.variable,
-        code.variable,
-        "scroll-smooth",
-        "antialiased"
-      )}
-      suppressHydrationWarning
-    >
-      <body className="relative flex flex-col min-h-screen bg-background font-sans transition-colors duration-300 overflow-hidden">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <TooltipProvider delayDuration={0}>
-            <Background/>
-            <Header dictionary={dict}/>
-            <main className="flex flex-grow">{children}</main>
-            <Footer dictionary={dict} />
-            <Toaster position="top-right" />
-          </TooltipProvider>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ViewTransitions>
+      <html
+        lang={lang}
+        className={cn(primary.variable, code.variable, 'scroll-smooth', 'antialiased')}
+        suppressHydrationWarning
+      >
+        <body className="relative flex flex-col min-h-screen bg-background font-sans transition-colors duration-300 overflow-hidden">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <TooltipProvider delayDuration={0}>
+              <ReactLenis root>
+                <Background />
+                <Header dictionary={dict} />
+                <main className="flex flex-grow">{children}</main>
+                <Footer dictionary={dict} />
+                <Toaster position="top-right" />
+              </ReactLenis >
+            </TooltipProvider>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ViewTransitions>
   );
 }
