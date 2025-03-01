a550d7febb1e593f7886f17431a932c0fcdea761
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2025-03-01 16:53:23 +0200

    feat(header): add hideOnScroll prop to Logo component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
-import { Logo } from "@/components/Logo";
-import { Dictionary } from "@/types/dictionary";
-import { Navbar } from "@/components/Navbar";
+import { Logo } from '@/components/Logo';
+import { Dictionary } from '@/types/dictionary';
+import { Navbar } from '@/components/Navbar';
 
 type HeaderProps = {
   dictionary: Dictionary;
@@ -9,7 +9,7 @@ type HeaderProps = {
 export const Header = ({ dictionary }: HeaderProps) => {
   return (
     <div className="z-50 flex items-center justify-between w-full max-w-4xl pt-4 fixed px-8 sm:px-16">
-      <Logo size="m" icon href="/" />
+      <Logo size="m" icon href="/" hideOnScroll />
       <Navbar dictionary={dictionary} />
     </div>
   );
