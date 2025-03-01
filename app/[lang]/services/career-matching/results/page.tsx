959aa6ca2852978e935f7d8d41242179bcebbef4
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2025-03-01 14:11:34 +0200

    fix(results): refactor Results page to correctly use dynamic dictionary loading

--- app/[lang]/services/career-matching/results/page.tsx
+++ app/[lang]/services/career-matching/results/page.tsx
@@ -1,13 +1,12 @@
-import { Dictionary } from '@/types/dictionary';
+import { getDictionary, SupportedLocale } from '../../../dictionaries';
 
-type ResultsProps = {
-  dictionary: Dictionary;
-};
-
-export function Results({ dictionary }: ResultsProps) {
+export default async function Results({ params }: { params: Promise<{ lang: SupportedLocale }> }) {
+  const lang = (await params).lang;
+  const dict = await getDictionary(lang);
   return (
     <div className="flex flex-col gap-4 items-center justify-center uppercase">
-      <h3>{dictionary.pageUnderConstruction}</h3>
+      <h3 className="text-2xl font-bold">
+        {dict.pageUnderConstruction}</h3>
     </div>
   );
 }
