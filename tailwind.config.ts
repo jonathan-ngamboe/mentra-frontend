80247bc5dc356649afcd5bbe3c03f5f0d00bf439
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2025-03-01 13:42:38 +0200

    feat(tailwind): add custom box shadow utilities for elevated and rainbow effects

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,6 +89,10 @@ module.exports = {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      boxShadow: {
+        'elevated': 'rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px',
+        'rainbow-steps': 'blue 0px 0px 0px 2px inset, rgb(255, 255, 255) 10px -10px 0px -3px, rgb(31, 193, 27) 10px -10px, rgb(255, 255, 255) 20px -20px 0px -3px, rgb(255, 217, 19) 20px -20px, rgb(255, 255, 255) 30px -30px 0px -3px, rgb(255, 156, 85) 30px -30px, rgb(255, 255, 255) 40px -40px 0px -3px, rgb(255, 85, 85) 40px -40px',
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
