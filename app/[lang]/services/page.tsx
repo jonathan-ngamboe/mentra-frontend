1ab234099cb398786a92ad8431cdd54205844017
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2025-03-06 23:17:19 +0200

    feat(services): replace FlowingMenu with WordScroller for service items display

--- app/[lang]/services/page.tsx
+++ app/[lang]/services/page.tsx
@@ -1,27 +1,33 @@
-import { getDictionary, SupportedLocale } from "../dictionaries";
-import { services } from "@/resources/config";
-import FlowingMenu from "@/components/reactbits/Components/FlowingMenu/FlowingMenu";
-import { getDictionaryValue } from "@/lib/utils";
+import { getDictionary, SupportedLocale } from '../dictionaries';
+import { services } from '@/resources/config';
+import WordScroller from '@/components/WordScroller';
+import { getDictionaryValue } from '@/lib/utils';
 
-export default async function Services({
-  params,
-}: {
-  params: Promise<{ lang: SupportedLocale }>;
-}) {
+export default async function Services({ params }: { params: Promise<{ lang: SupportedLocale }> }) {
   const lang = (await params).lang;
   const dict = await getDictionary(lang);
 
   function getServiceItems() {
     return services.map((service) => ({
-      text: getDictionaryValue(dict, service.labelKey) || service.labelKey,
-      image: service.image,
+      text:
+        getDictionaryValue(dict, service.labelKey).toLowerCase() || service.labelKey.toLowerCase(),
       link: service.link,
     }));
   }
 
   return (
-    <div className="relative w-full self-center">
-      <FlowingMenu items={getServiceItems()} className="text-foreground" />
+    <div className="container mx-auto self-center">
+      <div className="flex flex-col mx-auto">
+        <header className="flex items-center justify-center text-center min-h-screen">
+          <h1 className="text-5xl font-bold leading-tight">{dict.services.description}</h1>
+        </header>
+
+        <WordScroller prefix={dict.services.prefix} words={getServiceItems()} />
+
+        <section className="flex items-center justify-center min-h-screen">
+          <h2 className="text-5xl font-bold">{dict.services.end}.</h2>
+        </section>
+      </div>
     </div>
   );
 }
