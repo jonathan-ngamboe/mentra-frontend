5c911ee5098ddc4aa39d637ca624f457383b5f3b
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2025-04-13 10:24:45 +0300

    feat(component): add Loading component for loading state visualization

---
+++ components/Loading.tsx
@@ -0,0 +1,7 @@
+export function Loading() {
+  return (
+    <div className="flex items-center justify-center min-h-[120px] w-full">
+      <div className="w-8 h-8 border-2 border-transparent border-t-primary border-r-primary/70 border-b-primary/40 border-l-primary/10 rounded-full animate-spin"></div>
+    </div>
+  );
+}
