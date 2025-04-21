c9a219589b0e82cfc5b46ae308b6107ddab87a0c
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2025-04-21 22:31:04 +0300

    feat(types): add profession-related types for better type safety

---
+++ types/profession.ts
@@ -0,0 +1,35 @@
+import { RiasecDimension, RiasecScores } from '@/types/riasec';
+
+export type BaseProfession = {
+  id: number;
+  name: string;
+  option?: string | null;
+  description?: string | null;
+  activitySector?: string | null;
+  minDuration?: number | null;
+  maxDuration?: number | null;
+  effectiveDate?: string | null;
+};
+
+export type ProfessionMatch = Profession & {
+  correlation: number;
+};
+
+export type Profession = BaseProfession & {
+  riasecScores: RiasecScores[];
+};
+
+export type RawProfessionDbData = {
+  id: number;
+  name: string | null;
+  option?: string | null;
+  description?: string | null;
+  activity_sector?: string | null;
+  min_duration?: number | null;
+  max_duration?: number | null;
+  effective_date?: string | null;
+  riasec_evaluation: Array<{
+    score: number;
+    riasec_dimension: RiasecDimension[];
+  }>;
+};
