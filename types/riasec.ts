b4eb8d88956262fc8a8957c1641d94c12698c21c
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2025-04-21 22:31:27 +0300

    fix(types): remove redundant ProfessionMatch type definition and reorganize RiasecDimension type

--- types/riasec.ts
+++ types/riasec.ts
@@ -1,15 +1,16 @@
 import { Dictionary } from '@/types/dictionary';
+import { ProfessionMatch } from '@/types/profession';
+
+export type RiasecDimension = {
+  id: number;
+  name: string;
+  description?: string | null;
+};
 
 export type RiasecKey = 'R' | 'I' | 'A' | 'S' | 'E' | 'C';
 
 export type RiasecScores = {
   [key in RiasecKey]: number;
-};
-
-export type ProfessionMatch = {
-  id: number;
-  name: string;
-  correlation: number;
 };
 
 export type RiasecResultsProps = {
@@ -21,4 +22,13 @@ export type RiasecResultsProps = {
 export type RiasecResults = {
   profile: RiasecScores;
   professions: ProfessionMatch[];
+};
+
+export const dimensionNameToKeyMap: Record<string, keyof RiasecScores> = {
+  Realistic: 'R',
+  Investigative: 'I',
+  Artistic: 'A',
+  Social: 'S',
+  Enterprising: 'E',
+  Conventional: 'C',
 };
